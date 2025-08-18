5d28397ac6e011d8f9cbd9cea5ada304ca5495f4
Guilherme Martini <guilherme.dcmartini@gmail.com>
2025-08-17 22:31:52 -0300

    tentando fazer compilar

--- exercicios-beecrowd/iniciante/cartaCriptografada2502.c
+++ exercicios-beecrowd/iniciante/cartaCriptografada2502.c
@@ -12,17 +12,14 @@ int main(){
     scanf("%d %d", &c, &n);
     char cifra_primaria[c];
     char cifra_segundaria[c];
-    fgets(cifra_primaria, c, stdin);
-    fgets(cifra_segundaria, c, stdin);
+    scanf(" %s", cifra_primaria);
+    scanf(" %s", cifra_segundaria);
     for(int i = 0; i < n; i++){
         char frase[1000];
         fgets(frase, 1000, stdin);
         int i = 0;
         while(frase[i] != '\0'){
-            printf("%c\n", frase[i]);
-            for(int j = 0; j < c; j++){
-                printf("->%c\n", cifra_primaria[j]);
-                printf("->>%c\n", cifra_segundaria[j]);
+            for(int j = 0; j <= c; j++){
                 if(frase[i] == cifra_segundaria[j])
                     frase[i] = cifra_primaria[j];
                 if(frase[i] == cifra_primaria[j])
